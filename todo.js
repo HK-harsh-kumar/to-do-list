62e10344cbb0fd4564febd4f8a447869fb98c560
HK-harsh-kumar <129512543+HK-harsh-kumar@users.noreply.github.com>
2024-06-17 03:39:47 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,38 @@
+const inputbox= document.getElementById("inputbox");
+const listcontainer=document.getElementById("listcontainer");
+
+function addtask(){
+    if(inputbox.value===''){
+        alert("you must write something");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputbox.value;
+        listcontainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+        
+    }
+    inputbox.value='';
+    savedata();
+}
+
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagname==='li'){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagname==='span'){
+        e.target.parentNode.remove();
+    }
+    
+},false);
+
+function savedata(){
+    localStorage.setItem("data",listcontainer.innerHTML);
+}
+
+function showtask(){
+    listcontainer.innerHTML=localStorage.getItem("data");
+}
+showtask();
